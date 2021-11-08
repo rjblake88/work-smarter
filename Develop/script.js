1cdfbf6716fac215a22ea0c2fb054a776021d9a0
Ryan J Blake <ryanjblake1988@gmail.com>
2021-11-07 16:04:15 -0800

    setting up timeblocks in js

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+var $currentDay = $("#currentDay");
+var $timeBlocks = $(".time-block");
+var $scheduleArea = $(".schedule");
+var toDoItems = [];
+var currentDate = moment().format("dddd, MMMM Do");
+var currentHour = moment().format("H");
+//using moment.js to set date and time
+
+function initializeSchedule(){
+    $timeBlocks.each(function(){
+    var $thisBlock = $(this);
+    var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+    
+    var todoObj = {
+        hour: thisBlockHr,
+        text: "",
+        }
+       
+    toDoItems.push(todoObj);
+});
